ef1668ca69fcaabd530ab0240c56828d48b46190
Adele Angel <adeler.ang@gmail.com>
2026-07-22 22:01:41 +0200

    Register calculation router

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.routers.member_router import router as member_router
+from app.routers.calc_router import router as calc_router
 
 app = FastAPI()
 
@@ -8,3 +9,4 @@ def read_root():
     return {"message": "StructCalc API is running"}
 
 app.include_router(member_router)
+app.include_router(calc_router)
